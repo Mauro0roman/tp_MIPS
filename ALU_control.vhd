f2883a96c70d8f81dcccfaf296be9685f055b781
Mauro0roman <88685852+Mauro0roman@users.noreply.github.com>
2023-11-15 21:32:57 -0300

    Update ALU_control.vhd sintax

--- ALU_control.vhd
+++ ALU_control.vhd
@@ -8,38 +8,43 @@ entity ALU_control is
 			reset : in STD_LOGIC;
             inst : in STD_LOGIC_VECTOR (31 downto 0);
 			control_alu_op : in STD_LOGIC_VECTOR(2 downto 0);
-			alu_op : out STD_LOGIC_VECTOR(2 downto 0);)
+			alu_op : out STD_LOGIC_VECTOR(2 downto 0));
 end ALU_control; 
 
-Behavioral of ALU_control is
-    signal funct :std_logic_vector(5 downto 0);  
-begin
-    funct <= inst(5 downto 0);
+architecture Behavioral of ALU_control is
+begin   
     process(clk, reset, control_alu_op)
-        variable temp_alu_op: STD_LOGIC_VECTOR(2 downto 0);;
+        variable temp_alu_op: STD_LOGIC_VECTOR(2 downto 0);
     begin
         if (reset = "1") then
-            temp_alu_op <= "000"
-        elsif rising_edge(clk) then        
+            temp_alu_op := "000";       
+        else
             if (alu_op = "000") then
-                case funct is
+                case inst(5 downto 0) is
                     when "100000" =>
                         temp_alu_op := "010";
+
                     when "100010" =>
                         temp_alu_op := "110";
+
                     when "100100" =>
                         temp_alu_op := "000";
+
                     when "100101" =>
                         temp_alu_op := "001";
+
                     when "101010" =>
                         temp_alu_op := "111";
+
                     when others =>
                         temp_alu_op := "010";
-                end case;
+
+                    end case;
             else
                 temp_alu_op := control_alu_op;
             end if;
         end if;
+        
         alu_op <= temp_alu_op; 
     end process;
 end Behavioral;                       
